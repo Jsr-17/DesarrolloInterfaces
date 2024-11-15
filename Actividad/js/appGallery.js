e5c6590c6e7c068a9a814f2b50bceba876abae8d
Jsr-17 <jarmoreno5@gmail.com>
2024-11-15 13:36:52 +0100

    modal empezado

---
+++ Actividad/js/appGallery.js
@@ -0,0 +1,21 @@
+const image = document.querySelectorAll("img");
+const modalContainer = document.querySelector("#modalContainer");
+const contenedor = document.querySelector("#contenedor");
+
+let contenedorImagenes = [];
+for (let index = 0; index < image.length; index++) {
+  if (index == 0) {
+    continue;
+  }
+  contenedorImagenes = [...contenedorImagenes, image[index]];
+}
+contenedorImagenes.forEach((el) =>
+  el.addEventListener("click", (el) => muestraModal(el))
+);
+
+function muestraModal(elemento) {
+  const {
+    target: { src },
+  } = elemento;
+  contenedor.style.display = "none";
+}
